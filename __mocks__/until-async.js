b1d57f29def6ca01497f2b4374ca51d74b3ccfc3
Robin <robinbette@hotmail.com>
2026-01-13 09:39:38 +0100

    add some testing libraries

---
+++ __mocks__/until-async.js
@@ -0,0 +1,5 @@
+module.exports = {
+  until: () => ({
+    res: () => Promise.resolve(),
+  }),
+};
--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,20 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
 
+// Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jest-fixed-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
+    'until-async': '<rootDir>/__mocks__/until-async.js',
   },
 };
 
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
