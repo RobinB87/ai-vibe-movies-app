fccb4ddc10530ba7f97aa760f64edc02c03e3bb8
Robin <robinbette@hotmail.com>
2026-01-17 12:51:25 +0100

    add compare password func

--- app/api/auth/core/passwordHasher.ts
+++ app/api/auth/core/passwordHasher.ts
@@ -12,3 +12,8 @@ export function hashPassword(password: string, salt: string): Promise<string> {
 export function generateSalt(): string {
   return crypto.randomBytes(16).toString("hex").normalize();
 }
+
+export async function comparePasswords({password, salt, hashedPassword}: {password:string, salt:string, hashedPassword:string}) {
+  const inputHashedPassword = await hashPassword(password, salt);
+  return crypto.timingSafeEqual(Buffer.from(inputHashedPassword, "hex"), Buffer.from(hashedPassword, "hex"));
+}
